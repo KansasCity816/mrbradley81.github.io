3b65cbacd750b5b07855e077096104434279efa7
KansasCity816 <168231042+KansasCity816@users.noreply.github.com>
2024-11-26 17:51:35 -0600

    Create generateBlogList.js

---
+++ generateBlogList.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const blogFolder = './blogs'; // Folder containing Blog-Single files
+const outputFile = './Blog-List.html'; // File to update
+
+const generateBlogList = () => {
+  const blogFiles = fs.readdirSync(blogFolder).filter(file => file.endsWith('.html'));
+
+  let blogListHtml = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Blog List</title>
+  </head>
+  <body>
+    <h1>Our Blog</h1>
+    <ul>
+  `;
+
+  blogFiles.forEach(file => {
+    const title = file.replace(/-/g, ' ').replace('.html', ''); // Use file name as title
+    blogListHtml += `<li><a href="./blogs/${file}">${title}</a></li>`;
+  });
+
+  blogListHtml += `
+    </ul>
+  </body>
+  </html>
+  `;
+
+  fs.writeFileSync(outputFile, blogListHtml);
+  console.log('Blog-List.html updated successfully!');
+};
+
+generateBlogList();
